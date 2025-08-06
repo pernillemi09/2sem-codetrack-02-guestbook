0e84801bb51a21f3489f0f52dfcc42de982235fc
Martin Dilling-Hansen <madh@zealand.dk>
2025-08-06 17:17:04 +0200

    Added favicon

--- public/index.php
+++ public/index.php
@@ -172,6 +172,7 @@ if (!$debug_mode) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="favicon.png" type="image/png">
     <title>My Guestbook</title>
     <style>
         body {
